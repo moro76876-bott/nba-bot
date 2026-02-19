a01edabad447728e3f99eabc0aa4e1b865e81d4c
moro76876-bott <moro76876@gmail.com>
2026-02-19 11:56:43 +0200

    clean start

---
+++ main.py
@@ -0,0 +1,88 @@
+
+import discord
+import requests
+import asyncio
+import feedparser
+import os
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
+last_tweet = None
+
+load_dotenv()
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+API_KEY = os.getenv("SPORTSDATA_API_KEY")
+
+print("API KEY:", API_KEY)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+
+seen = set()
+
+from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup
+
+from bs4 import BeautifulSoup
+
+
+
+def get_latest_tweet():
+    url = "https://nitter.net/UnderdogNBA/rss"
+
+    feed = feedparser.parse(url)
+
+    if feed.entries:
+        entry = feed.entries[0]
+        return entry.title, entry.link
+
+    return None, None
+
+
+
+
+
+
+@client.event
+async def on_ready():
+    print(f"✅ Logged in as {client.user}")
+    client.loop.create_task(loop())
+
+async def loop():
+    await client.wait_until_ready()
+    channel = client.get_channel(CHANNEL_ID)
+
+    global last_tweet
+
+    while True:
+        try:
+            # 👇 PUT IT HERE
+            tweet_text, tweet_link = get_latest_tweet()
+
+            if tweet_link and tweet_link != last_tweet:
+                last_tweet = tweet_link
+
+                embed = discord.Embed(
+                    title="🐦 NBA Alert",
+                    description=tweet_text,
+                    color=0x1DA1F2
+                )
+
+                embed.url = tweet_link
+                embed.set_footer(text="@UnderdogNBA")
+
+                await channel.send(embed=embed)
+
+        except Exception as e:
+            print("ERROR:", e)
+
+        await asyncio.sleep(30)
+
+
+
+
+
+        
+client.run(TOKEN)
